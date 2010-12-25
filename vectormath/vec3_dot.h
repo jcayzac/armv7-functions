398d9ed9d3d8a6527765004db0b57c3bb4a6676e
Julien Cayzac <julien.cayzac@gmail.com>
2010-12-25 15:59:47 +0900

    better vec3_dot and vec4_dot

--- vectormath/vec3_dot.h
+++ vectormath/vec3_dot.h
@@ -2,19 +2,15 @@
 #include <armv7-functions/common.h>
 
 ARMV7_FUNC_API void vec3_dot(float& result, const float32x4_t& a, const float32x4_t& b) {
-	register float32x4_t _0;
-	register int _1;
-	register float32x4_t ta = a; //ta = vsetq_lane_f32(0.f, ta, 3);
-	register float32x4_t tb = b; //tb = vsetq_lane_f32(0.f, tb, 3);
+	register float32x4_t tmp;
+	register const int zero(0);
 	asm volatile (
-	"# dot product of %q2.xyz with %q3.xyz:\n\t"
-	"vmul.f32  %e0, %e2, %e3\n\t"
-	"movs            %1, #0\n\t"
-	"vmov.32     %f2[1], %1\n\t"
-	"vmov.32     %f3[1], %1\n\t"
-	"vmla.f32  %e0, %f2, %f3\n\t"
-	"vpadd.f32 %e0, %e0\n\t"
-	: "=&w" (_0), "=&r" (_1), "=&w" (ta), "=&w" (tb) :: );
-	result = vgetq_lane_f32(_0, 0);
+	"# dot product of %q[a].xyz with %q[b].xyz: \n\t"
+	"vmul.f32   %q[tmp],    %q[a],   %q[b]      \n\t"
+	"vmov.32    %f[tmp][1],	%[zero]             \n\t"
+	"vadd.f32   %e[tmp],    %e[tmp], %f[tmp]    \n\t"
+	"vpadd.f32  %e[tmp],    %e[tmp]             \n\t"
+	: [tmp] "=&w" (tmp) : [a] "w" (a), [b] "w" (b), [zero] "r" (zero): );
+	result = vgetq_lane_f32(tmp, 0);
 }
 
--- vectormath/vec4_dot.h
+++ vectormath/vec4_dot.h
@@ -4,11 +4,11 @@
 ARMV7_FUNC_API void vec4_dot(float& result, const float32x4_t& a, const float32x4_t& b) {
 	register float32x4_t tmp;
 	asm volatile (
-	"# dot product of %q1.xyzw with %q2.xyzw:\n\t"
-	"vmul.f32  %e0, %e1, %e2\n\t"
-	"vmla.f32  %e0, %f1, %f2\n\t"
-	"vpadd.f32 %e0, %e0\n\t"
-	: "=&w" (tmp) : "w" (a), "w" (b) : );
+	"# dot product of %q[a].xyzw with %q[b].xyzw: \n\t"
+	"vmul.f32   %q[tmp],    %q[a],   %q[b]        \n\t"
+	"vadd.f32   %e[tmp],    %e[tmp], %f[tmp]      \n\t"
+	"vpadd.f32  %e[tmp],    %e[tmp]               \n\t"
+	: [tmp] "=&w" (tmp) : [a] "w" (a), [b] "w" (b) : );
 	result = vgetq_lane_f32(tmp, 0);
 }
 
